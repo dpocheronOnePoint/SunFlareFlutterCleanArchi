312d8211830a09eba03156dd8f2bfef3820e23b1
your_username <your_email@gmail.com>
2022-07-07 09:12:04 +0200

    Initialize project

---
+++ android/app/src/main/kotlin/com/example/sun_flare_clean_archi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sun_flare_clean_archi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:sun_flare_clean_archi/application/app.dart';
+
+void main() {
+  runApp(Application());
+}
